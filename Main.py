9717511d22e40d8817c9eaff97b4c02535dae75a
Srinivas Nagidi <srinivas.nagidi@gmail.com>
2023-10-05 15:45:44 +0530

    change file

--- Main.py
+++ Main.py
@@ -1,8 +1,14 @@
 from fastapi import FastAPI
 
 app = FastAPI()
+<<<<<<< Updated upstream
 //
 // fast api cooking
+=======
+
+#fast api cooking
+#edit
+>>>>>>> Stashed changes
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
